162836c57a8c1a5a6d5e231d04ab65e04950bc5a
csci3251-1155127407 <1155127407@link.cuhk.edu.hk>
2020-09-27 06:46:51 +0800

    z

--- codeitsuisse/routes/bucket_fill.py
+++ codeitsuisse/routes/bucket_fill.py
@@ -10,11 +10,13 @@ logger = logging.getLogger(__name__)
 @app.route('/bucket-fill', methods=['POST'])
 def evaluate_bucket():
     data = request.get_data(as_text=True)
-    logging.info("data sent for evaluation {}".format(data))
+    # logging.info("data sent for evaluation {}".format(data))
 
+    print("="*88)
+    print(data)
     result = {"result" : 2754}
 
-    logging.info("My result :{}".format(result))
+    # logging.info("My result :{}".format(result))
     return json.dumps(result);
 
 
