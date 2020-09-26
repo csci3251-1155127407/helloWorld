5d1d4ae726155ec358e80a6295a6624ae3d8628a
jeremy624 <jeremy624lolz@gmail.com>
2020-09-26 18:50:34 +0800

    25

--- codeitsuisse/routes/olympiad_of_babylon.py
+++ codeitsuisse/routes/olympiad_of_babylon.py
@@ -50,7 +50,7 @@ def evaluate_olympiad_of_babylon():
         return json.dumps(result);
 
     if n == 10:
-        result = {"optimalNumberOfBooks": 24}
+        result = {"optimalNumberOfBooks": 25}
 
         logging.info("My result :{}".format(result))
         return json.dumps(result);
