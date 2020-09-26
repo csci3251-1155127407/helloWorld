a48ec52531edba84780cd23ebd570c8cbdc11c9a
jeremy624 <jeremy624lolz@gmail.com>
2020-09-26 12:59:05 +0800

    fruit fix

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -11,15 +11,15 @@ logger = logging.getLogger(__name__)
 def evaluate_fruitbasket():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    apple = data.get('maApple')
-    watermelon = data.get('maWatermelon')
-    banana = data.get('maBanana')
-
-    a = 10
-    w = 20
-    b = 30
-
-    result = a * apple + w * watermelon + b * banana
+    # apple = data.get('maApple')
+    # watermelon = data.get('maWatermelon')
+    # banana = data.get('maBanana')
+    #
+    # a = 10
+    # w = 20
+    # b = 30
+    #
+    # result = a * apple + w * watermelon + b * banana
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
