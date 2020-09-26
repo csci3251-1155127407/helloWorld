582b7a3e50dc25ba4bdc7b3d74d7659c44d519f4
csci3251-1155127407 <1155127407@link.cuhk.edu.hk>
2020-09-27 03:47:07 +0800

    zzz

--- codeitsuisse/routes/bored_scribe.py
+++ codeitsuisse/routes/bored_scribe.py
@@ -115,7 +115,7 @@ def evaluate_bored_scribe():
             if not (f in vis):
                 vis[f] = True
             else:
-                cnt = 1
+                cnt = 0
                 break
             f = rot(f, res3 + sum(ord(f[j]) for j in range(res1, res2 + 1)))
             # print(f)
