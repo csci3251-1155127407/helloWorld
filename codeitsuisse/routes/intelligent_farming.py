ce9c306f27d4ef30e78ee2850c896ef73930ee8c
csci3251-1155127407 <1155127407@link.cuhk.edu.hk>
2020-09-26 13:21:29 +0800

    Implement farming

--- codeitsuisse/routes/intelligent_farming.py
+++ codeitsuisse/routes/intelligent_farming.py
@@ -15,33 +15,80 @@ def evaluate_intelligent_farming():
     result = {"runId": data["runId"], "list": []}
     tests = data["list"]
 
-    max_len = 0
-
     for test in tests:
         result["list"] += [{"id": test["id"]}]
         s = test["geneSequence"]
-        max_len = max(max_len, len(s))
 
+        cnt_a = cnt_c = cnt_g = cnt_t = 0
+        for ch in s:
+            if (ch == 'A'):
+                cnt_a += 1
+            if (ch == 'C'):
+                cnt_c += 1
+            if (ch == 'G'):
+                cnt_g += 1
+            if (ch == 'T'):
+                cnt_t += 1
 
-        # cnt_a = cnt_c = cnt_g = cnt_t = 0
-        # for ch in s:
-        #     if (ch == 'A'):
-        #         cnt_a += 1
-        #     if (ch == 'C'):
-        #         cnt_c += 1
-        #     if (ch == 'G'):
-        #         cnt_g += 1
-        #     if (ch == 'T'):
-        #         cnt_t += 1
+        print(cnt_c)
+        num_cc = 0
+        num_acgt = 0
+        max_score = 0
+        for i in range(cnt_c // 2 + 1):
+            print("i =", i)
+            cnt_acgt = min(cnt_a, cnt_c - i * 2, cnt_g, cnt_t)
+            for j in range(cnt_acgt + 1):
+                score_cc = i * 25
+                score_acgt = j * 15
+                
+                rem_a = cnt_a - j
+                rem_c = cnt_c - i * 2 - j
+                rem_g = cnt_g - j
+                rem_t = cnt_t - j
 
-        # num_cc = 0
-        # max_score = 0
-        # for i in range(cnt_c // 2):
-        #     score = i * 25
-        #     score += min(cnt_a, cnt_c - i * 2, cnt_g, cnt_t) * 15
-        #     if (score > max_score):
-        #         max_score = score
-        #         num_cc = i
+                rem_a -= i * 2
+                rem_a -= (rem_c + rem_g + rem_t) * 2
+                rem_a = max(0, rem_a)
+
+                score_aaa = rem_a // 3 * 20
+
+                if (score_cc + score_acgt - score_aaa > max_score):
+                    max_score = score_cc + score_acgt - score_aaa
+                    num_cc = i
+                    num_acgt = j
+
+        rem_a = cnt_a - num_acgt
+        rem_c = cnt_c - num_cc * 2 - num_acgt
+        rem_g = cnt_g - num_acgt
+        rem_t = cnt_t - num_acgt
+
+        ans = "ACGT" * num_acgt
+
+        for i in range(num_cc):
+            if (rem_a >= 2):
+                ans += "AA"
+                rem_a -= 2
+            ans += "CC"
+
+        for i in range(rem_c):
+            if (rem_a >= 2):
+                ans += "AA"
+                rem_a -= 2
+            ans += "C"
+
+        for i in range(rem_g):
+            if (rem_a >= 2):
+                ans += "AA"
+                rem_a -= 2
+            ans += "G"
+
+        for i in range(rem_t):
+            if (rem_a >= 2):
+                ans += "AA"
+                rem_a -= 2
+            ans += "T"
+
+        ans += "A" * rem_a
 
         # num_acgt = min(cnt_a, cnt_c - num_cc * 2, cnt_g, cnt_t)
         # num_a = cnt_a - num_acgt
@@ -57,9 +104,10 @@ def evaluate_intelligent_farming():
         #     ans += "A"
 
         # ans += "A" * num_a
-        # result["list"][-1]["geneSequence"] = ans
 
-    print(max_len)
+        result["list"][-1]["geneSequence"] = ans
+
+        print(max_score)
 
     logging.info("My result :{}".format(result))
     return json.dumps(result)
