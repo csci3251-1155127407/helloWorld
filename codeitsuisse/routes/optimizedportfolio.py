ecf1a818d69a8c09cdf31f9ec801bd98e7bdcb10
jeremy624 <jeremy624lolz@gmail.com>
2020-09-26 21:14:15 +0800

    It clicked

--- codeitsuisse/routes/optimizedportfolio.py
+++ codeitsuisse/routes/optimizedportfolio.py
@@ -36,15 +36,33 @@ def evaluate_optimizedportfolio():
         best_x = 9e18
         best_y = 9e18
 
+        # lowest future vasidjhasd
+        best1 = ""
+        best_x1 = 9e18
+        best_y1 = 9e18
+        best_z1 = 9e18
 
         for i in range(len(index_futures)):
             x, y = calc(index_futures[i])
+            z = index_futures[i]["FuturePrcVol"]
+
             print(F"x: {x}, y: {y}")
-            if ((x < best_x) or (x == best_x and y < best_y)):
+            if (x < best_x or (x == best_x and y < best_y)):
                 best = index_futures[i]["Name"]
                 best_x = x
                 best_y = y
 
+            if (z < best_z1 or (z == best_z1 and y < best_y1)):
+                best1 = index_futures[i]["Name"]
+                best_x1 = x
+                best_y1 = y
+                best_z1 = z
+
+
+        if best_y1 < best_y:
+            best = best1
+            best_x = best_x1
+            best_y = best_y1
 
         result["outputs"] += [{"HedgePositionName": best, "OptimalHedgeRatio": round(best_x, 3), "NumFuturesContract": round(best_y)}]
 
