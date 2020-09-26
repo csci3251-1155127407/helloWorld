7ad8088442b51f7124c3b3d8d04f890d8c91957e
csci3251-1155127407 <1155127407@link.cuhk.edu.hk>
2020-09-26 18:19:51 +0800

    Add EPS

--- codeitsuisse/routes/optimizedportfolio.py
+++ codeitsuisse/routes/optimizedportfolio.py
@@ -37,7 +37,8 @@ def evaluate_optimizedportfolio():
         for i in range(len(index_futures)):
             x, y = calc(index_futures[i])
             print(F"x: {x}, y: {y}")
-            if ((best_x == -1 or x < best_x) or (x == best_x and y < best_y)):
+            EPS = 1e-6
+            if ((best_x == -1 or x < best_x) or (abs(x - best_x) < EPS and y < best_y)):
                 best = index_futures[i]["Name"]
                 best_x = x
                 best_y = y
