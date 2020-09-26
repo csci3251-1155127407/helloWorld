194742b5a9bb6395c57208eceabc02f4fa25fc19
csci3251-1155127407 <1155127407@link.cuhk.edu.hk>
2020-09-26 23:52:28 +0800

    diudiu

--- codeitsuisse/routes/optimizedportfolio2.py
+++ codeitsuisse/routes/optimizedportfolio2.py
@@ -22,7 +22,8 @@ def calc(index_future):
     x = portfolio["SpotPrcVol"] * index_future["CoRelationCoefficient"] / index_future["FuturePrcVol"]
     x = my_round(x, 3)
     y = x * portfolio["Value"] / (index_future["IndexFuturePrice"] * index_future["Notional"])
-    y = my_round(y)
+    # x = max(x, 0)
+    # x = min(x, 1)
     return x, y
 
 @app.route('/optimizedportfolio', methods=['POST'])
@@ -36,16 +37,17 @@ def evaluate_optimizedportfolio():
         global index_futures
         portfolio = test["Portfolio"]
         index_futures = test["IndexFutures"]
+
         best = ""
         best_x = 9e18
         best_y = 9e18
         for i in range(len(index_futures)):
             x, y = calc(index_futures[i])
-            print(F"x: {x}, y: {y}")
+            # print(F"x: {x}, y: {y}")
             if ((x < best_x) or (x == best_x and y < best_y)):
                 best = index_futures[i]["Name"]
                 best_x = x
                 best_y = y
-        result["outputs"] += [{"HedgePositionName": best, "OptimalHedgeRatio": best_x, "NumFuturesContract": best_y}]
+        result["outputs"] += [{"HedgePositionName": best, "OptimalHedgeRatio": my_round(best_x, 3), "NumFuturesContract": my_round(best_y)}]
     logging.info("My result :{}".format(result))
     return json.dumps(result)
